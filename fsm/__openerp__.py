5dc0fbc9eb93e08c630f2f17fff9746febb08726
François Degrave <francois.degrave@fundp.ac.be>
2016-10-03 10:38:07 +0200

    Category + icon

--- fsm/__openerp__.py
+++ fsm/__openerp__.py
@@ -3,10 +3,11 @@
     "version": "1.0",
     "depends": ["base", "mail"],
     "author": "University of Namur",
-    "category": "Backend",
+    "category": "Extra Tools",
     "description": """A lightweight, declarative, Pythonic replacement for (simple) workflows""",
     'data': [],
     'installable': True,
     'application': True,
     'auto_install': False,
+    'license': 'AGPL-3'
 }
